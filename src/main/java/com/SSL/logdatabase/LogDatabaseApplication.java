29691614ad86405c54dc5d8e4fa54df2a1e2e8de
Renyi <jianghurainxwy@gmail.com>
2023-04-02 19:05:25 +0200

    initialize

---
+++ src/main/java/com/SSL/logdatabase/LogDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.SSL.logdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SSL/logdatabase/LogDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSL.logdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
